805f63d88871ebe2592968ec45fb6b809ba345f6
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 11:46:12 +0100

    Removed counter functionality

--- src/cas-frontend/src/app/counter/counter-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core'
-
-@Component({
-  selector: 'counter-view',
-  templateUrl: 'counter-view.component.html'
-})
-export class CounterViewComponent{
-  @Input('counter-view-counter') parentInput;
-}
