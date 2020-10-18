e17b8282e73e9cfe8dc95ea715b7e42f74a54dad
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 10:17:57 +0100

    Removed testing decorator code

--- src/cas-frontend/src/app/app.component.ts
+++ src/cas-frontend/src/app/app.component.ts
@@ -1,22 +1,5 @@
 import { Component } from '@angular/core'
 
-function TestDecorator (text) {
-  name: 'A simple test decorator'
-    return (component) => {
-      component.data = text;
-      component.meta = {
-      prop1: 'text1',
-      prop2: 'text2'
-    }
-    console.log(text)
-    console.log(component.meta.prop1)
-    console.log(component.meta.prop2) 
-  }
-}
-
-@TestDecorator({
-  text: 'Text from test decorator'
-})
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -25,9 +8,5 @@ function TestDecorator (text) {
 
 export class AppComponent {
   title = 'Hello I am an angular component.'
-  testDecoratorName = `${AppComponent.data.text} ${AppComponent['meta'].prop1} ${AppComponent['meta'].prop2}`;
-  generateNumber () {
-    return 12
-  }
 }
 
