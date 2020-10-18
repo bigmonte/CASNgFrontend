7c508ad2d4e8ec9aa9eaba43351823fe54476278
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 12:19:43 +0100

    Imported bootstrap for prototyping frontend

--- src/cas-frontend/src/app/app.module.ts
+++ src/cas-frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
+import { CommandComponent } from './commands/command.component'
 
 
 @NgModule({
   // [components; directives or pipes]  (declarables) that belongs to this module
-  declarations: [AppComponent],
+  declarations: [AppComponent, CommandComponent],
   // set of NgModules whose exported declarables are available to template
   // in this module
   imports: [BrowserModule],
