6a713292235d8b5380573463046fc7674665ad00
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 09:50:28 +0100

    Hello world Angular Component

---
+++ src/cas-frontend/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
+
+@NgModule({
+  // [components; directives or pipes]  (declarables) that belongs to this module
+  declarations: [AppComponent],
+  // set of NgModules whose exported declarables are available to template
+  // in this module
+  imports: [BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
