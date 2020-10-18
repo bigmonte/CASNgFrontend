f122b707f995bdce5e325c514c55b9889973ce97
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 10:12:41 +0100

    Accessing decorator data from component class

--- src/cas-frontend/src/app/app.component.ts
+++ src/cas-frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core'
 
 function TestDecorator (text) {
+  name: 'A simple test decorator'
   return function (target) {
-    target.text = text;
+    target.data = text;
     target.meta = {
       prop1: 'text1',
       prop2: 'text2'
@@ -13,7 +14,9 @@ function TestDecorator (text) {
   }
 }
 
-@TestDecorator('Simple test')
+@TestDecorator({
+  text: 'Text from test decorator'
+})
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +25,7 @@ function TestDecorator (text) {
 
 export class AppComponent {
   title = 'Hello I am an angular component.'
+  testDecoratorName = `${AppComponent.data.text} ${AppComponent['meta'].prop1} ${AppComponent['meta'].prop2}`;
   generateNumber () {
     return 12
   }
