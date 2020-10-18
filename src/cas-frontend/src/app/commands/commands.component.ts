ca65291d3a07e739fc7e1682343506b3262b2180
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 17:13:06 +0100

    Adding command prototype functionality

--- src/cas-frontend/src/app/commands/commands.component.ts
+++ src/cas-frontend/src/app/commands/commands.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { Command } from './models/Command'
+import { Command } from './models/Command'
 
 
 
@@ -8,13 +8,17 @@ import { Command } from './models/Command'
   templateUrl: 'commands.component.html',
 })
 export class CommandsComponent {
-   public commands: Command[] = [
-     { id: 1, commandLine: "dotnet ef ", description: "Print EF Help", platform: ".NET Core EF CLI" },
-     { id: 2, commandLine: "dotnet build", description: "Build dotnet Project", platform: ".NET Core CLI" },
-     { id: 3, commandLine: "dotnet run", description: "Run dotnet Project", platform: ".NET Core CLI" },
-     { id: 4, commandLine: "dotnet new", description: "Show help in creating projects", platform: ".NET Core CLI" },
-     { id: 5, commandLine: "dotnet ef migrations add", description: "Code first migration", platform: ".NET Core EF CLI" },
-    ]
-  
+  public commands: Command[] = [
+    { id: 1, commandLine: "dotnet ef ", description: "Print EF Help", platform: ".NET Core EF CLI" },
+    { id: 2, commandLine: "dotnet build", description: "Build dotnet Project", platform: ".NET Core CLI" },
+    { id: 3, commandLine: "dotnet run", description: "Run dotnet Project", platform: ".NET Core CLI" },
+    { id: 4, commandLine: "dotnet new", description: "Show help in creating projects", platform: ".NET Core CLI" },
+    { id: 5, commandLine: "dotnet ef migrations add", description: "Code first migration", platform: ".NET Core EF CLI" },
+  ]
+
+  public addCommand() {
+    let command: Command = { id: 1, commandLine: "dotnet ef ", description: "Print EF Help", platform: ".NET Core EF CLI" }
+    this.commands.unshift(command)
+  }
 }
 
