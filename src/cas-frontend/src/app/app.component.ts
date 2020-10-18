f951792c3702c8bc4c263b5ac475d52305cd39bb
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 10:05:20 +0100

    Creating a test Decorator

--- src/cas-frontend/src/app/app.component.ts
+++ src/cas-frontend/src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core'
 
+function TestDecorator (text) {
+  return function (target) {
+    target.text = text;
+    target.meta = {
+      prop1: 'text1',
+      prop2: 'text2'
+    }
+    console.log(text)
+    console.log(target.meta.prop1)
+    console.log(target.meta.prop2) 
+  }
+}
+
+@TestDecorator('Simple test')
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,3 +26,4 @@ export class AppComponent {
     return 12
   }
 }
+
